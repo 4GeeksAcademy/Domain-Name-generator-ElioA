856aa27ca3e234dd984fd9e98cc43452c4b45d19
ElioAloise <155248934+ElioAloise@users.noreply.github.com>
2024-02-11 20:04:50 +0000

    my first project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //pretier no please
+  let art = ["the", "our", "an", "some", "this"];
+  let adj = ["great", "big", "cool", "awesome", "funny"];
+  let nouns = ["jogger", "Racoon", "website", "rocket", "Farm"];
+  let Tlds = [".com", ".net", ".org", ".pt"];
+
+  let randomDomainNames = [];
+
+  for (let article of art) {
+    for (let adjetive of adj) {
+      for (let noun of nouns) {
+        for (let tld of Tlds) {
+          randomDomainNames.push(article + adjetive + noun + tld);
+        }
+      }
+    }
+  }
+
+  document.body.innerHTML = `<ul>${randomDomainNames
+    .map(randomDomainNames => `<li>${randomDomainNames}</li>`)
+    .join("")}
+  </ul>`;
 };
